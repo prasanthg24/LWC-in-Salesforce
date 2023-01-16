74d3546bbba379ee03ee3d9b926fe2cbc28c9211
prasanthg24 <prasanth287471@gmail.com>
2023-01-16 19:47:25 +0530

    code updated

--- force-app/main/default/lwc/lightningRecordwithFormLayout/lightningRecordwithFormLayout.js
+++ force-app/main/default/lwc/lightningRecordwithFormLayout/lightningRecordwithFormLayout.js
@@ -1,4 +1,28 @@
 import { LightningElement } from 'lwc';
-export default class LightningRecordwithFormLayout extends LightningElement {
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import {NavigationMixin} from 'lightning/navigation';
+export default class LightningRecordwithFormLayout extends NavigationMixin(LightningElement)
+{
+
+    handleSuccess(event)
+    {
+        const evt = new ShowToastEvent({ 
+        title: "Account Created",
+        message: "Record Id -"+event.detail.id,
+        variant: "success"
+        });
+        this.dispatchEvent(evt);
+
+        this[NavigationMixin.Navigate]({
+   
+             type: 'standard__recordPage',
+            attributes:{
+                recordId : event.detail.id,
+                objectApiName:'Account',
+                actionName : 'view'
+            },
+       
+    });
+    }
 
 }

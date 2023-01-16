c64a14224ac2ddb4d0341838ca55cffb7807e057
prasanthg24 <prasanth287471@gmail.com>
2023-01-16 20:41:12 +0530

    Quick Contact Component code updated

---
+++ force-app/main/default/lwc/quickContactCreate/quickContactCreate.js
@@ -0,0 +1,28 @@
+import { LightningElement,api } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import FName_field from '@salesforce/schema/Contact.FirstName';
+import  LName_field from '@salesforce/schema/Contact.LastName';
+import Email from '@salesforce/schema/Contact.Email';
+import Phone_field  from '@salesforce/schema/Contact.Phone';
+export default class QuickContactCreate extends LightningElement 
+{
+    contactFields = [ FName_field,LName_field,Email,Phone_field];
+    @api recordId;
+    handleSuccess(event)
+    {
+        const evt = new ShowToastEvent({ 
+        title: "Contact  Created",
+        message: "Record Id -"+event.detail.id,
+        variant: "success"
+        });
+        this.dispatchEvent(evt);
+    }
+    handleSubmit(event)
+    {
+        event.preventDefault();
+        const fields = event.detail.fields;
+        fields.AccountId = this.recordId;
+        this.template.querySelector('lightning-record-form').submit(fields);
+        eval("$A.get('e.force:refreshView').fire();");
+    }
+}

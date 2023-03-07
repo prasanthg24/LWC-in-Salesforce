aa7aabca8139802e04a9035d94e1043850ae3436
prasanthg24 <prasanth287471@gmail.com>
2023-03-07 06:03:21 +0530

    Quick Contact  Load Component using UIrecAPI

--- force-app/main/default/lwc/quickContactLoad_uiAPI/quickContactLoad_uiAPI.js
+++ force-app/main/default/lwc/quickContactLoad_uiAPI/quickContactLoad_uiAPI.js
@@ -1,7 +1,12 @@
 import { LightningElement,api,wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
 
 export default class QuickContactLoad_uiAPI extends LightningElement {
 
+    @api recordId;
+    contact;
+    name;
+    phone;
 
 
 }

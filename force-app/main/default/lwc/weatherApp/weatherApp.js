016f2d1e629dd232ba72954bd6b8c2e98b8c508d
prasanthg24 <prasanth287471@gmail.com>
2023-04-11 20:28:26 +0530

    Lwc weather App  project  updated

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -1,5 +1,19 @@
 import { LightningElement } from 'lwc';
+const API_KEY ='95a829aa49a520ba92a7146cee5282b3'
 export default class WeatherApp extends LightningElement 
 {
-
+    cityName =' '
+    searchHandler(event)
+    {
+        this.cityName = event.target.value
+    }
+    submitHandler(event)
+    {
+      event.preventDefault();
+      this.fetchData();
+    }
+    fetchData ()
+    {
+        console.log(this.cityName);
+    }
 }

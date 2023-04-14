ec8c55e1a50e8f516f0ab4453d00b134701f665d
prasanthg24 <prasanth287471@gmail.com>
2023-04-14 19:56:59 +0530

    Lwc weather App  project  updated

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -3,19 +3,63 @@ const API_KEY ='95a829aa49a520ba92a7146cee5282b3'
 export default class WeatherApp extends LightningElement 
 {
     cityName =' '
+    loadingText = ' '
+    isError=false
+
+ get loadingClasses(){
+    return this.isError ? 'error-msg':'success-msg'
+  }
+
+
     searchHandler(event)
     {
         this.cityName = event.target.value
     }
+
     submitHandler(event)
     {
       event.preventDefault();
       this.fetchData();
     }
-    fetchData ()
+
+      fetchData ()
     {
+        this.isError=false
+        this.loadingText ="Fetching weather data..."
         console.log(this.cityName);
-        //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-        https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+       
+
+        const URL =`https://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&units=metric&appid=${API_KEY}`
+        
+        fetch(URL)
+        .then(res => res.json())
+        .then(result => {
+            console.log(JSON.stringify(result));
+            this.weatherDetails(result)
+        })
+        .catch(error => {
+            this.isError=true
+            console.error(error);
+            console.error("something went wrong");
+              this.loadingText ="something went wrong..."
+        })
+
+           
+
     }
+
+     weatherDetails(info)
+    {
+    if(info.cod === "404")
+    {
+      this.isError = true 
+      this.loadingText = `${this.cityName} isn't a valid city name`
+    } else 
+    {
+      this.loadingText = ''
+    }
+    }
+
+ 
+
 }
